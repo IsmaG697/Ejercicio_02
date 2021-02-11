78b9708738e28a4602fb959f44c4fec4b2cf9a04
Ismael García <igjw2000@gmail.com>
2021-02-11 01:32:23 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    int num = 0;
+
+    Console.WriteLine ("Números del 1-10: ");
+
+    while (num <= 9) {
+
+
+      num++;
+
+      Console.Write(num);
+
+    }
+
+    Console.ReadKey();
+
+  }
+}
